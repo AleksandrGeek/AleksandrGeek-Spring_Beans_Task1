d76365f52daa81b06db1a57cdc8629270990597b
AleksandrGeek <slaytribal@gmail.com>
2025-11-19 11:41:09 +0300

    Initial commit with Spring beans project

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,27 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class AnimalsCage {
 
+    @Qualifier("dog")
     @Autowired
     private Animal animal;
-
+Timer timer = new Timer();
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+ 
+
+    public Timer getTimer() {
+        return timer;
     }
 }
